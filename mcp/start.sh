d6e8b0aeb8ad6ef6029c47cac519e966f0df407b
williamyzd <williamyzd@163.com>
2025-05-04 09:41:42 +0000

    desktop调试成功

--- mcp/start.sh
+++ mcp/start.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-# 检查日志目录是否存在
-
-# nohup  python3 $WEB_DIR/start_chrome.py --port 8000 > $WEB_DIR/log/brower.log 2>&1 &
-
-#nohup npx -y @playwright/mcp@latest --port 8002 --executable-path /usr/bin/chromium --user-data-dir ${BROWER_USER_DIR}   >  ${WEB_DIR}/log/playwright-mcp.log 2>&1 &
-nohup npx -y @playwright/mcp@latest --port 8002  --user-data-dir ${BROWER_USER_DIR}   >  ${WEB_DIR}/log/playwright-mcp.log 2>&1 &
+#!/usr/bin/env bash
+nohup npx -y @playwright/mcp@latest --port 8002 --executable-path /usr/bin/chromium --user-data-dir ${BROWER_USER_DIR}   >  ${WEB_DIR}/log/playwright-mcp.log 2>&1 &
+# nohup npx -y @playwright/mcp@latest --port 8002  --user-data-dir ${BROWER_USER_DIR}   >  ${WEB_DIR}/log/playwright-mcp.log 2>&1 &
 
 sleep 5
-. $WEB_PYTHON_ENV/bin/activate
-nohup mcpo --port 8001 --config ${${MCP_CONFIG}}/config.json > ${WEB_DIR}/log/mcp.log 2>&1 &
+nohup $WEB_PYTHON_ENV/bin/python $WEB_PYTHON_ENV/bin/mcpo --port 8001 --config ${MCP_CONFIG}/config.json > ${WEB_DIR}/log/mcp.log 2>&1 &
 # tail -f   /var/log/mcp.log
